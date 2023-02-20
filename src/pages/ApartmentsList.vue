80fea8aee0050d79f0984708e1c4665d610d011f
LuisJann <giannoneluigi10@gmail.com>
2023-02-20 21:00:29 +0100

    Co-authored-by: gioanbatan <gioanbatan@users.noreply.github.com>

--- src/pages/ApartmentsList.vue
+++ src/pages/ApartmentsList.vue
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <template>
-    <main class="py-5">
+    <main class="bg-color">
 
         <!-- <div class="container">
             <LoadingSpinner size="large" v-if="this.$store.isLoading"/>
@@ -34,45 +34,86 @@ export default {
                 </div>
             </div>
         </div> -->
-        <hr>
-        <div class="container d-flex">
-            <div class="apartment-img">
-                <img src="../assets/appartament/appartamento1.jpg" alt="">
-            </div>
-            <div class="text-side">
-                <h4>Prova</h4>
-                <hr>
-                <div class="details">
-                    <p>Letti:4 - camere da letto:3 - bagni:1 - WIFI- Posto Macchina</p>
+        <div class="wrapper mt-1" v-for="apartment in this.$store.apartments">
+            <router-link :to="{ name: 'single-apartment', params: { slug: apartment.slug } }" href=""> 
+            <div class="container d-flex">
+                    <div class="apartment-img">
+                        <img src="../assets/appartament/appartamento1.jpg" alt="">
+                    </div>
+                    <div class="text-side">
+                        <h2>{{apartment.title}}</h2>
+                        <hr>
+                        <div class="details text-white">
+                            <p>Stanze <i class="fa-solid fa-house"></i>: <strong>{{ apartment.room_number}}</strong> <br> Letti <i class="fa-solid fa-bed"></i>: <strong>{{ apartment.bed_number }}</strong> <br> bagni <i class="fa-solid fa-toilet"></i>: <strong>{{ apartment.bathroom_number }}</strong></p>
+                            <p>Indirizzo: {{ apartment.full_address }}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </router-link>
         </div>
-        <hr>
+        <div class="pagination-bg d-flex justify-content-center pt-4">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item disabled">
+                        <a class="page-link">Previous</a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">Next</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </main>
 </template>
 
 <style lang="scss">
+
+.pagination-bg{
+    background-color: rgb(24, 24, 24)!important;
+}
+.bg-color{
+    background-color: #c9e265;
+}
+.wrapper{
+    background-color: rgb(24, 24, 24);
+    padding: 10px 0 10px 0;
+
+}
+.contianer{
+    border-radius:15px ;
+}
+.wrapper:hover{
+    background-color: rgb(53, 53, 53);
+}
 .apartment-img{
-    width: 30%;
+    width: 60%;
     margin-right: 10px;
+
     img{
-        border-radius: 5px;
+        border-radius: 15px;
 
     }
 }
 .text-side{
     margin: 5px 0 5px 0;
+    width: 40%;
     hr{
-        width: 20%;
+        // width: 60%;
     }
     .details{
-        width: 50%;
-        font-size: small;
-        color: grey;
+        font-size: large;
+        color: white;
     }
 }
 
 hr{
     border: 1px solid #c9e265;
 }
+
+h2{
+        color: #c9e265;
+    }
 </style>
